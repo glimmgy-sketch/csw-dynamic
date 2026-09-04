79185ed0fa5a9217e11ae24e8fdad414b81d552f
glimmgy-sketch <glim.mgy@gmail.com>
2026-09-05 02:07:42 +0630

    Update models.py

--- models.py
+++ models.py
@@ -16,7 +16,8 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
+    media_file = db.Column(db.String(200), nullable=True)
+
     likes = db.relationship('Like', backref='post', lazy=True, cascade="all, delete-orphan")
     comments = db.relationship('Comment', backref='post', lazy=True, cascade="all, delete-orphan")
 
